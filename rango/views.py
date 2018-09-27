71cef2645f114863dd3f0b0522e013ac5bfa5156
jah1603 <hendersonjamesalexander@gmail.com>
2018-09-28 00:10:37 +0100

    about page rendering

--- rango/views.py
+++ rango/views.py
@@ -7,3 +7,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('Rango says hello!')
+
+def aboutpage(request):
+    return HttpResponse('Rango says: "Here is the about page."')
