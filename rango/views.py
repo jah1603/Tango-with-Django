56bdca7f2f6a1381eda3001bcd363d57de1f2d92
jah1603 <hendersonjamesalexander@gmail.com>
2018-09-28 16:53:39 +0100

    renders list of categories on home page

--- rango/views.py
+++ rango/views.py
@@ -7,13 +7,16 @@ from django.shortcuts import render_to_response
 
 from django.http import HttpResponse
 
+from rango.models import Category
+
 def index(request):
     # Request the context of the request.
 # The context contains information such as the client's machine details, for example.
     context = RequestContext(request)
 # Construct a dictionary to pass to the template engine as its context.
 # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "I am bold font from the context"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
 # Return a rendered response to send to the client.
 # We make use of the shortcut function to make our lives easier.
 # Note that the first parameter is the template we wish to use.
