e38178f809516c05fac694fd643d59fd0d1836d5
jah1603 <hendersonjamesalexander@gmail.com>
2018-09-28 16:36:48 +0100

    modifies display of pages to show more headings

--- rango/admin.py
+++ rango/admin.py
@@ -4,7 +4,13 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from rango.models import Category, Page
 
-admin.site.register(Category)
-admin.site.register(Page)
-
 # Register your models here.
+
+class PageAdmin(admin.ModelAdmin):
+     list_display = ('title', 'category', 'url')
+
+    # def __unicode__(self):
+    #     return self.name
+
+admin.site.register(Category)
+admin.site.register(Page, PageAdmin)
