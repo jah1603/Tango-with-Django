e8687299b1065a923d8cd6ae62515742fa215121
jah1603 <hendersonjamesalexander@gmail.com>
2018-09-28 15:41:23 +0100

    updates relational database with views and likes for categories

--- populate_rango.py
+++ populate_rango.py
@@ -5,7 +5,7 @@ django.setup()
 from rango.models import Category, Page
 
 def populate():
-    python_cat = add_cat('Python')
+    python_cat = add_cat('Python', 128, 64)
 
     add_page(cat=python_cat,
         title="Official Python Tutorial",
@@ -19,7 +19,7 @@ def populate():
         title="Learn Python in 10 Minutes",
         url="http://www.korokithakis.net/tutorials/python/")
 
-    django_cat = add_cat("Django")
+    django_cat = add_cat("Django", 64, 32)
 
     add_page(cat=django_cat,
         title="Official Django Tutorial",
@@ -33,7 +33,7 @@ def populate():
         title="How to Tango with Django",
         url="http://www.tangowithdjango.com/")
 
-    frame_cat = add_cat("Other Frameworks")
+    frame_cat = add_cat("Other Frameworks", 32 , 16)
 
     add_page(cat=frame_cat,
         title="Bottle",
@@ -52,7 +52,7 @@ def add_page(cat, title, url, views=0):
     p = Page.objects.get_or_create(category=cat, title=title, url=url, views=views)[0]
     return p
 
-def add_cat(name):
+def add_cat(name, views, likes):
     c = Category.objects.get_or_create(name=name)[0]
     return c
 
