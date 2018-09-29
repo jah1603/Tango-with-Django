a9663529007bcfe12fafc0950080c7477375e4a5
jah1603 <hendersonjamesalexander@gmail.com>
2018-09-29 21:05:34 +0100

    removes problematic access restriction for adding a page

--- rango/views.py
+++ rango/views.py
@@ -12,6 +12,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 from rango.models import Category, Page, UserProfile
 from rango.forms import CategoryForm, PageForm
